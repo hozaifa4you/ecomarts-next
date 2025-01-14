9d06c6914b7f756fd2876ded0c875f7787a2ca37
Hozaifa Bin Masud <hozaifabinmasud@gmail.com>
2025-01-14 13:10:18 +0600

    Reactive intor 3

--- src/components/Homepage3/IntroSection.tsx
+++ src/components/Homepage3/IntroSection.tsx
@@ -1,4 +1,11 @@
-import React from "react";
+/* eslint-disable @next/next/no-img-element */
+"use client";
+import Image from "next/image";
+import Link from "next/link";
+import { Autoplay, Pagination } from "swiper/modules";
+import { Swiper, SwiperSlide } from "swiper/react";
+
+import { introData, IntroDataType } from "@/utils/data/homepage-3";
 
 const IntroSection = () => {
    return (
@@ -26,132 +33,28 @@ const IntroSection = () => {
                <div className="intro-wrapper style3">
                   <div className="row gy-5 d-flex align-items-center">
                      <div className="col-xl-9">
-                        <div className="swiper hero-slider-3">
-                           <div className="swiper-wrapper">
-                              <div className="swiper-slide">
-                                 <div className="row g-4">
-                                    <div className="col-lg-7">
-                                       <div className="intro-content">
-                                          <h6 className="intro-subtitle style3">
-                                             100% Organic Product
-                                          </h6>
-                                          <h1 className="intro-title">
-                                             Choose your Healthy Lifestyle
-                                          </h1>
-                                          <p>
-                                             Nunc lectus dui, ultricies id orci
-                                             ac, condimentum posuere sapien.
-                                             <br />
-                                             ipsum primis in faucibus orci
-                                             luctus et ultrices.
-                                          </p>
-                                          <div className="btn-wrapper">
-                                             <a
-                                                className="theme-btn style4"
-                                                href="cart.html"
-                                             >
-                                                Buy Now
-                                                <i className="fa-light fa-chevron-right"></i>
-                                             </a>
-                                          </div>
-                                       </div>
-                                    </div>
-                                    <div className="col-lg-5">
-                                       <div className="intro-thumb">
-                                          <img
-                                             src="/images/intro/introThumb3_1.png"
-                                             alt="thumb"
-                                          />
-                                       </div>
-                                    </div>
-                                 </div>
-                              </div>
-                              <div className="swiper-slide">
-                                 <div className="row g-4">
-                                    <div className="col-lg-7">
-                                       <div className="intro-content">
-                                          <h6 className="intro-subtitle style3">
-                                             100% Organic Product
-                                          </h6>
-                                          <h1 className="intro-title">
-                                             Choose your Healthy Lifestyle
-                                          </h1>
-                                          <p>
-                                             Nunc lectus dui, ultricies id orci
-                                             ac, condimentum posuere sapien.
-                                             <br />
-                                             ipsum primis in faucibus orci
-                                             luctus et ultrices.
-                                          </p>
-                                          <div className="btn-wrapper">
-                                             <a
-                                                className="theme-btn style4"
-                                                href="cart.html"
-                                             >
-                                                Buy Now
-                                                <i className="fa-light fa-chevron-right"></i>
-                                             </a>
-                                          </div>
-                                       </div>
-                                    </div>
-                                    <div className="col-lg-5">
-                                       <div className="intro-thumb">
-                                          <img
-                                             src="/images/intro/introThumb3_1.png"
-                                             alt="thumb"
-                                          />
-                                       </div>
-                                    </div>
-                                 </div>
-                              </div>
-                              <div className="swiper-slide">
-                                 <div className="row g-4">
-                                    <div className="col-lg-7">
-                                       <div className="intro-content">
-                                          <h6 className="intro-subtitle style3">
-                                             100% Organic Product
-                                          </h6>
-                                          <h1 className="intro-title">
-                                             Choose your Healthy Lifestyle
-                                          </h1>
-                                          <p>
-                                             Nunc lectus dui, ultricies id orci
-                                             ac, condimentum posuere sapien.
-                                             <br />
-                                             ipsum primis in faucibus orci
-                                             luctus et ultrices.
-                                          </p>
-                                          <div className="btn-wrapper">
-                                             <a
-                                                className="theme-btn style4"
-                                                href="cart.html"
-                                             >
-                                                Buy Now
-                                                <i className="fa-light fa-chevron-right"></i>
-                                             </a>
-                                          </div>
-                                       </div>
-                                    </div>
-                                    <div className="col-lg-5">
-                                       <div className="intro-thumb">
-                                          <img
-                                             src="/images/intro/introThumb3_1.png"
-                                             alt="thumb"
-                                          />
-                                       </div>
-                                    </div>
-                                 </div>
-                              </div>
-                           </div>
+                        <Swiper
+                           loop
+                           pagination={{ clickable: true }}
+                           autoplay
+                           modules={[Pagination, Autoplay]}
+                        >
+                           {introData.map((item) => (
+                              <SwiperSlide key={item.id}>
+                                 <IntroSlide {...item} />
+                              </SwiperSlide>
+                           ))}
                            <div className="swiper-dot2 text-center">
                               <div className="dot2"></div>
                            </div>
-                        </div>
+                        </Swiper>
                      </div>
                      <div className="col-xl-3">
                         <div className="intro-second-thumb">
                            <div className="thumb">
-                              <img
+                              <Image
+                                 width={335}
+                                 height={430}
                                  src="/images/intro/intro-thumb3_2.jpg"
                                  alt="Thumb"
                               />
@@ -161,13 +64,13 @@ const IntroSection = () => {
                               <h4>75% OFF</h4>
                               <p>Only Fruit & Vegetable</p>
                               <div className="btn-wrapper">
-                                 <a
+                                 <Link
                                     className="theme-btn style4"
-                                    href="cart.html"
+                                    href="/cart"
                                  >
                                     Buy Now
                                     <i className="fa-sharp fa-regular fa-arrow-right"></i>
-                                 </a>
+                                 </Link>
                               </div>
                            </div>
                         </div>
@@ -177,6 +80,31 @@ const IntroSection = () => {
             </div>
          </div>
       </section>
+   );
+};
+
+const IntroSlide = ({ description, img, subtitle, title }: IntroDataType) => {
+   return (
+      <div className="row g-4">
+         <div className="col-lg-7">
+            <div className="intro-content">
+               <h6 className="intro-subtitle style3">{subtitle}</h6>
+               <h1 className="intro-title">{title}</h1>
+               <p dangerouslySetInnerHTML={{ __html: description }}></p>
+               <div className="btn-wrapper">
+                  <Link className="theme-btn style4" href="/cart">
+                     Buy Now
+                     <i className="fa-light fa-chevron-right"></i>
+                  </Link>
+               </div>
+            </div>
+         </div>
+         <div className="col-lg-5">
+            <div className="intro-thumb">
+               <Image width={425} height={285} src={img} alt="thumb" />
+            </div>
+         </div>
+      </div>
    );
 };
 
---
+++ src/utils/data/homepage-3.ts
@@ -0,0 +1,26 @@
+const introData = [
+   {
+      id: 1,
+      subtitle: "100% Organic Product",
+      title: "Choose your Healthy Lifestyle",
+      description: `Nunc lectus dui, ultricies id orci ac, condimentum posuere sapien. <br /> ipsum primis in faucibus orci luctus et ultrices.`,
+      img: "/images/intro/introThumb3_1.png",
+   },
+   {
+      id: 2,
+      subtitle: "100% Organic Product",
+      title: "Choose your Healthy Lifestyle",
+      description: `Nunc lectus dui, ultricies id orci ac, condimentum posuere sapien. <br /> ipsum primis in faucibus orci luctus et ultrices.`,
+      img: "/images/intro/introThumb3_1.png",
+   },
+   {
+      id: 3,
+      subtitle: "100% Organic Product",
+      title: "Choose your Healthy Lifestyle",
+      description: `Nunc lectus dui, ultricies id orci ac, condimentum posuere sapien. <br /> ipsum primis in faucibus orci luctus et ultrices.`,
+      img: "/images/intro/introThumb3_1.png",
+   },
+];
+
+export type IntroDataType = (typeof introData)[0];
+export { introData };
