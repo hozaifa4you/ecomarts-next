fee91997c3cb5185caf89576bd58c9199d247532
Hozaifa Bin Masud <hozaifabinmasud@gmail.com>
2025-01-25 18:12:56 +0600

    Reactive of blog section 5

--- src/components/Homepage5/BlogSection.tsx
+++ src/components/Homepage5/BlogSection.tsx
@@ -1,3 +1,4 @@
+import Image from "next/image";
 import React from "react";
 
 const BlogSection = () => {
@@ -8,7 +9,12 @@ const BlogSection = () => {
                <div className="col-xl-4 col-md-6">
                   <div className="blog-card-items-four style-2">
                      <div className="blog-card-items-four__thumb">
-                        <img src="/images/home-5/banner/04.jpg" alt="thumb" />
+                        <Image
+                           width={470}
+                           height={309}
+                           src="/images/home-5/banner/04.jpg"
+                           alt="thumb"
+                        />
                      </div>
                      <div className="blog-card-items-four__content style-two pt-0 mb-0">
                         <span>New collection</span>
@@ -24,27 +30,37 @@ const BlogSection = () => {
                   </div>
                </div>
                <div className="col-xl-4 col-md-6">
-                  <div className="blog-card-items-four__content style-two pb-0 mt-0">
-                     <span>New collection</span>
-                     <h3>Wedding Rings</h3>
-                     <p>
-                        Praesent ut lacus at tincidunt aliquam a eget urna. Sed
-                        ullamcorper
-                     </p>
-                     <a href="shop-details-one.html" className="theme-btn">
-                        Discover More
-                     </a>
-                  </div>
                   <div className="blog-card-items-four style-2">
+                     <div className="blog-card-items-four__content style-two pt-0 mb-0">
+                        <span>New collection</span>
+                        <h3>Wedding Rings</h3>
+                        <p>
+                           Praesent ut lacus at tincidunt aliquam a eget urna.
+                           Sed ullamcorper
+                        </p>
+                        <a href="shop-details-one.html" className="theme-btn">
+                           Discover More
+                        </a>
+                     </div>
                      <div className="blog-card-items-four__thumb">
-                        <img src="/images/home-5/banner/05.jpg" alt="thumb" />
+                        <Image
+                           width={470}
+                           height={309}
+                           src="/images/home-5/banner/05.jpg"
+                           alt="thumb"
+                        />
                      </div>
                   </div>
                </div>
                <div className="col-xl-4 col-md-6">
                   <div className="blog-card-items-four style-2">
                      <div className="blog-card-items-four__thumb">
-                        <img src="/images/home-5/banner/06.jpg" alt="thumb" />
+                        <Image
+                           width={470}
+                           height={309}
+                           src="/images/home-5/banner/06.jpg"
+                           alt="thumb"
+                        />
                      </div>
                      <div className="blog-card-items-four__content style-two pt-0 mb-0">
                         <span>New collection</span>
