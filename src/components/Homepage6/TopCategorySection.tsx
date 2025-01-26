002ecaf57302407e7b0d4fda1b749a21334c09a7
Hozaifa Bin Masud <hozaifabinmasud@gmail.com>
2025-01-26 17:27:08 +0600

    Reactive top category 6

--- src/components/Homepage6/TopCategorySection.tsx
+++ src/components/Homepage6/TopCategorySection.tsx
@@ -1,4 +1,7 @@
-import React from "react";
+import Image from "next/image";
+import Link from "next/link";
+
+import { topCategoryData, TopCategoryType } from "@/utils/data/homepage-6";
 
 const TopCategorySection = () => {
    return (
@@ -10,192 +13,33 @@ const TopCategorySection = () => {
                   <h2>Shop by Top Categories</h2>
                </div>
                <div className="row g-4">
-                  <div className="col-xl-2 col-lg-4 col-md-6 col-sm-6">
-                     <div className="product-box-items-three style-2">
-                        <div className="product-image">
-                           <img
-                              src="/images/home-6/product/01.png"
-                              alt="icon"
-                           />
-                        </div>
-                        <div className="product-content">
-                           <h6>
-                              <a href="shop-details-one.html">Armchair</a>
-                           </h6>
-                        </div>
+                  {topCategoryData.map((category) => (
+                     <div
+                        className="col-xl-2 col-lg-4 col-md-6 col-sm-6"
+                        key={category.id}
+                     >
+                        <TopCategoryCard {...category} />
                      </div>
-                  </div>
-                  <div className="col-xl-2 col-lg-4 col-md-6 col-sm-6">
-                     <div className="product-box-items-three style-2">
-                        <div className="product-image">
-                           <img
-                              src="/images/home-6/product/02.png"
-                              alt="icon"
-                           />
-                        </div>
-                        <div className="product-content">
-                           <h6>
-                              <a href="shop-details-one.html">Ceiling Fan</a>
-                           </h6>
-                        </div>
-                     </div>
-                  </div>
-                  <div className="col-xl-2 col-lg-4 col-md-6 col-sm-6">
-                     <div className="product-box-items-three style-2">
-                        <div className="product-image">
-                           <img
-                              src="/images/home-6/product/03.png"
-                              alt="icon"
-                           />
-                        </div>
-                        <div className="product-content">
-                           <h6>
-                              <a href="shop-details-one.html">Chandelier</a>
-                           </h6>
-                        </div>
-                     </div>
-                  </div>
-                  <div className="col-xl-2 col-lg-4 col-md-6 col-sm-6">
-                     <div className="product-box-items-three style-2">
-                        <div className="product-image">
-                           <img
-                              src="/images/home-6/product/04.png"
-                              alt="icon"
-                           />
-                        </div>
-                        <div className="product-content">
-                           <h6>
-                              <a href="shop-details-one.html">Ceiling Fan</a>
-                           </h6>
-                        </div>
-                     </div>
-                  </div>
-                  <div className="col-xl-2 col-lg-4 col-md-6 col-sm-6">
-                     <div className="product-box-items-three style-2">
-                        <div className="product-image">
-                           <img
-                              src="/images/home-6/product/05.png"
-                              alt="icon"
-                           />
-                        </div>
-                        <div className="product-content">
-                           <h6>
-                              <a href="shop-details-one.html">Cushion</a>
-                           </h6>
-                        </div>
-                     </div>
-                  </div>
-                  <div className="col-xl-2 col-lg-4 col-md-6 col-sm-6">
-                     <div className="product-box-items-three style-2">
-                        <div className="product-image">
-                           <img
-                              src="/images/home-6/product/06.png"
-                              alt="icon"
-                           />
-                        </div>
-                        <div className="product-content">
-                           <h6>
-                              <a href="shop-details-one.html">Armchairn</a>
-                           </h6>
-                        </div>
-                     </div>
-                  </div>
-                  <div className="col-xl-2 col-lg-4 col-md-6 col-sm-6">
-                     <div className="product-box-items-three style-2">
-                        <div className="product-image">
-                           <img
-                              src="/images/home-6/product/07.png"
-                              alt="icon"
-                           />
-                        </div>
-                        <div className="product-content">
-                           <h6>
-                              <a href="shop-details-one.html">Dining Room</a>
-                           </h6>
-                        </div>
-                     </div>
-                  </div>
-                  <div className="col-xl-2 col-lg-4 col-md-6 col-sm-6">
-                     <div className="product-box-items-three style-2">
-                        <div className="product-image">
-                           <img
-                              src="/images/home-6/product/08.png"
-                              alt="icon"
-                           />
-                        </div>
-                        <div className="product-content">
-                           <h6>
-                              <a href="shop-details-one.html">Clockwatcher</a>
-                           </h6>
-                        </div>
-                     </div>
-                  </div>
-                  <div className="col-xl-2 col-lg-4 col-md-6 col-sm-6">
-                     <div className="product-box-items-three style-2">
-                        <div className="product-image">
-                           <img
-                              src="/images/home-6/product/09.png"
-                              alt="icon"
-                           />
-                        </div>
-                        <div className="product-content">
-                           <h6>
-                              <a href="shop-details-one.html">Glass Vase</a>
-                           </h6>
-                        </div>
-                     </div>
-                  </div>
-                  <div className="col-xl-2 col-lg-4 col-md-6 col-sm-6">
-                     <div className="product-box-items-three style-2">
-                        <div className="product-image">
-                           <img
-                              src="/images/home-6/product/10.png"
-                              alt="icon"
-                           />
-                        </div>
-                        <div className="product-content">
-                           <h6>
-                              <a href="shop-details-one.html">
-                                 Handmade Pillow
-                              </a>
-                           </h6>
-                        </div>
-                     </div>
-                  </div>
-                  <div className="col-xl-2 col-lg-4 col-md-6 col-sm-6">
-                     <div className="product-box-items-three style-2">
-                        <div className="product-image">
-                           <img
-                              src="/images/home-6/product/11.png"
-                              alt="icon"
-                           />
-                        </div>
-                        <div className="product-content">
-                           <h6>
-                              <a href="shop-details-one.html">Armchair</a>
-                           </h6>
-                        </div>
-                     </div>
-                  </div>
-                  <div className="col-xl-2 col-lg-4 col-md-6 col-sm-6">
-                     <div className="product-box-items-three style-2">
-                        <div className="product-image">
-                           <img
-                              src="/images/home-6/product/12.png"
-                              alt="icon"
-                           />
-                        </div>
-                        <div className="product-content">
-                           <h6>
-                              <a href="shop-details-one.html">Ceiling Fan</a>
-                           </h6>
-                        </div>
-                     </div>
-                  </div>
+                  ))}
                </div>
             </div>
          </div>
       </section>
+   );
+};
+
+const TopCategoryCard = ({ img, name }: TopCategoryType) => {
+   return (
+      <div className="product-box-items-three style-2">
+         <div className="product-image">
+            <Image src={img} width={130} height={130} alt="ecomarts-category" />
+         </div>
+         <div className="product-content">
+            <h6>
+               <Link href="/shop-details-one">{name}</Link>
+            </h6>
+         </div>
+      </div>
    );
 };
 
---
+++ src/utils/data/homepage-6.ts
@@ -0,0 +1,17 @@
+const topCategoryData = [
+   { id: 1, img: "/images/home-6/product/01.png", name: "Armchair" },
+   { id: 2, img: "/images/home-6/product/02.png", name: "Ceiling Fan" },
+   { id: 3, img: "/images/home-6/product/03.png", name: "Chandelier" },
+   { id: 4, img: "/images/home-6/product/04.png", name: "Coffee Table" },
+   { id: 5, img: "/images/home-6/product/05.png", name: "Desk Lamp" },
+   { id: 6, img: "/images/home-6/product/06.png", name: "Floor Lamp" },
+   { id: 7, img: "/images/home-6/product/07.png", name: "Loveseat" },
+   { id: 8, img: "/images/home-6/product/08.png", name: "Nightstand" },
+   { id: 9, img: "/images/home-6/product/09.png", name: "Recliner" },
+   { id: 10, img: "/images/home-6/product/10.png", name: "Sectional Sofa" },
+   { id: 11, img: "/images/home-6/product/11.png", name: "Side Table" },
+   { id: 12, img: "/images/home-6/product/12.png", name: "Wall Art" },
+];
+
+export type TopCategoryType = (typeof topCategoryData)[0];
+export { topCategoryData };
