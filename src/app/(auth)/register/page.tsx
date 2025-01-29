5b5f3c4c93eaa8c44e9bc070de0a0fe9684eb285
Hozaifa Bin Masud <hozaifabinmasud@gmail.com>
2025-01-29 18:26:11 +0600

    Reactive of register page

---
+++ src/app/(auth)/register/page.tsx
@@ -0,0 +1,157 @@
+import Image from "next/image";
+import { Breadcrumb } from "@/components/Breadcrumb";
+import Link from "next/link";
+
+const RegisterPage = () => {
+   return (
+      <>
+         <Breadcrumb mainTitle="Register" secondaryTitle="Category" />
+
+         <section className="register-section fix section-padding">
+            <div className="container">
+               <div className="register-wrapper">
+                  <div className="row gx-5">
+                     <div className="col-xl-6 offset-xl-0 col-md-8 offset-md-2">
+                        <div className="contact-info-area">
+                           <div className="contact-content">
+                              <h2 className="contact-content__title">
+                                 Get Started Now
+                              </h2>
+                              <p className="contact-content__subtitle">
+                                 Enter your Credentials to access your account
+                              </p>
+                              <form
+                                 action="contact.php"
+                                 id="contact-form"
+                                 method="POST"
+                                 className="contact-form-items"
+                              >
+                                 <div className="row g-4">
+                                    <div
+                                       className="col-lg-12 wow fadeInUp"
+                                       data-wow-delay=".3s"
+                                    >
+                                       <div className="form-clt">
+                                          <span>Your name*</span>
+                                          <input
+                                             type="text"
+                                             name="name"
+                                             id="name"
+                                             placeholder="Enter your name"
+                                          />
+                                       </div>
+                                    </div>
+                                    <div
+                                       className="col-lg-12 wow fadeInUp"
+                                       data-wow-delay=".5s"
+                                    >
+                                       <div className="form-clt">
+                                          <span>Email address*</span>
+                                          <input
+                                             type="text"
+                                             name="email"
+                                             id="email"
+                                             placeholder="Enter your email"
+                                          />
+                                       </div>
+                                    </div>
+                                    <div
+                                       className="col-lg-12 wow fadeInUp"
+                                       data-wow-delay=".7s"
+                                    >
+                                       <div className="form-clt">
+                                          <span>Password*</span>
+                                          <input
+                                             type="text"
+                                             name="number"
+                                             id="password"
+                                             placeholder="********"
+                                          />
+                                       </div>
+                                    </div>
+                                    <div className="form-check">
+                                       <div className="">
+                                          <input
+                                             id="reviewcheck"
+                                             name="reviewcheck"
+                                             type="checkbox"
+                                          />
+                                          <label
+                                             className="form-check-label"
+                                             htmlFor="reviewcheck"
+                                          >
+                                             I agree to the{" "}
+                                             <span>terms & policy</span>
+                                          </label>
+                                       </div>
+                                    </div>
+                                    <div
+                                       className="col-lg-12 wow fadeInUp"
+                                       data-wow-delay=".9s"
+                                    >
+                                       <button
+                                          type="submit"
+                                          className="theme-btn style6"
+                                       >
+                                          Sign Up
+                                       </button>
+                                    </div>
+                                 </div>
+                              </form>
+
+                              <div className="or-border">
+                                 <div className="border"></div>
+                                 <p>Or</p>
+                              </div>
+
+                              <div className="button-items-1">
+                                 <Link href="#!">
+                                    <div className="button-items-1__google-button">
+                                       <Image
+                                          width={20}
+                                          height={20}
+                                          src="/images/register/googleIcon.png"
+                                          alt="icon"
+                                       />
+                                       <p>Sign in with Google</p>
+                                    </div>
+                                 </Link>
+
+                                 <Link href="#!">
+                                    <div className="button-items-1__apple-button">
+                                       <Image
+                                          width={20}
+                                          height={20}
+                                          src="/images/register/appleIcon.png"
+                                          alt="icon"
+                                       />
+                                       <p>Sign in with Apple</p>
+                                    </div>
+                                 </Link>
+                              </div>
+                              <h5 className="contact-content__logtitle center">
+                                 Have an account?{" "}
+                                 <Link href="/login">Sign Up</Link>
+                              </h5>
+                           </div>
+                        </div>
+                     </div>
+                     <div className="col-xl-6 offset-xl-0 col-md-8 offset-md-2">
+                        <div className="register-thumb">
+                           <Image
+                              width={670}
+                              height={955}
+                              src="/images/register/loginThumb.jpg"
+                              alt="register-thumb"
+                           />
+                        </div>
+                     </div>
+                  </div>
+               </div>
+            </div>
+         </section>
+      </>
+   );
+};
+
+export default RegisterPage;
